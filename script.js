03a1e319726a0845717958aba6297bb12e846ca1
maraisaferreira <ferreira.maraisacristina@gmail.com>
2021-02-03 11:03:35 -0300

    updated

---
+++ script.js
@@ -0,0 +1,38 @@
+const container = document.getElementById('img_container');
+const loader = document.getElementById('loader');
+const apiUrl = 'http://localhost:9090/nasa-photos';
+
+/* Função auxiliar pra setar os attrs nos elem DOM */
+function setAttributes(element, attributes) {
+	for (const key in attributes) {
+		element.setAttribute(key, attributes[key]);
+	}
+}
+
+(async () => {
+	/* abre o arq JSON */
+	const response = await fetch(apiUrl);
+	const data = await response.json();
+	let photosArray = data.result;
+
+	photosArray.forEach((photo) => {
+		const image = document.createElement('img');
+		setAttributes(image, {
+			src: photo.src,
+			alt: "Nasa's Photo",
+			title: "Nasa's Photo",
+		});
+
+		image.addEventListener('click', () => {
+			/* abre o Insta da NASA numa outra guia */
+			window.open(
+				'https://facebook.com/pg/nasasolarsystem/photos/?tab=album&album_id=164320877917',
+				'_blank'
+			);
+		});
+
+		container.appendChild(image);
+	});
+
+	loader.hidden = true;
+})();
